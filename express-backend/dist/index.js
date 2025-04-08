76312ee96d33b5b79075bdf476be10eeecb868ae
Martin John M <johnmmartin74@gmail.com>
2025-04-08 13:48:52 +0200

    added start server logic

--- express-backend/dist/index.js
+++ express-backend/dist/index.js
@@ -17,32 +17,17 @@ const redis_1 = require("redis");
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 const client = (0, redis_1.createClient)();
-client.on('error', (err) => console.log('Redis Client Error', err));
-app.post("/submit", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const problemId = req.body.problemId;
-    const code = req.body.code;
-    const language = req.body.language;
-    try {
-        yield client.lPush("problems", JSON.stringify({ code, language, problemId }));
-        // Store in the database
-        res.status(200).send("Submission received and stored.");
-    }
-    catch (error) {
-        console.error("Redis error:", error);
-        res.status(500).send("Failed to store submission.");
-    }
-}));
 function startServer() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
             yield client.connect();
-            console.log("Connected to Redis");
+            console.log("conncected to redis");
             app.listen(3000, () => {
-                console.log("Server is running on port 3000");
+                console.log("server is running on 3000");
             });
         }
-        catch (error) {
-            console.error("Failed to connect to Redis", error);
+        catch (e) {
+            console.error("failed to connct to redis", e);
         }
     });
 }
